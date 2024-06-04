553e239311ed39e1eff8be2b097910ad4d20ce34
Stefano Bianda <stefanobianda@ik.me>
2024-06-04 17:39:56 +0200

    show an image instead of a textr

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -19,8 +19,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
       ),
     );
   }
